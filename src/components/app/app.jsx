0b9b2d373e73b52d796290d9d94345375f8042e9
Anastasiia Konstantinovich <konstantinovicha@extyl-pro.ru>
2019-11-19 19:53:33 +0300

    Adding state, App was transformed from function to class

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,18 +2,33 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
 
-const App = (props) => {
-  const {gameTime, errorCount, questions} = props;
-  return <WelcomeScreen
-    time={gameTime}
-    errorCount={errorCount}
-    questions={questions}
-  />;
-};
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      question: -1,
+    };
+  }
+  render() {
+    const {gameTime, errorCount, questions} = this.props;
+    return <WelcomeScreen
+      time={gameTime}
+      errorCount={errorCount}
+      onStartButtonClick = {() => {
+        this.setState((prevState) => ({
+          ...prevState,
+          question: prevState.question + 1,
+        }))
+      }}
+    />;
+  }
+}
 
 App.propTypes = {
   gameTime: PropTypes.number,
   errorCount: PropTypes.number,
+  questions: PropTypes.object
 };
 
 export default App;
