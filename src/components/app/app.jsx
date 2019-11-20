4d483d7fabd6485541623c1e82b1b3a0eacd8b90
Anastasiia Konstantinovich <konstantinovicha@extyl-pro.ru>
2019-11-20 17:48:23 +0300

    State for app, question screen in process

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
+import GenreQuestionScreen from '../genre-question-screen/genre-question-screen.jsx';
 
 class App extends React.PureComponent {
+  static getScreen(question, props, onUserAnswer) {
+    if (question === -1) {
+      const {gameTime, errorCount} = props;
+      return <WelcomeScreen
+        time = {gameTime}
+        errorCount = {errorCount}
+        onStartButtonClick = {onUserAnswer}
+      />;
+    }
+    const {questions} = props;
+    return <GenreQuestionScreen
+      question = {questions[question]}
+      onAnswer = {onUserAnswer}
+    />;
+  }
   constructor(props) {
     super(props);
 
@@ -10,25 +26,25 @@ class App extends React.PureComponent {
       question: -1,
     };
   }
+
   render() {
     const {gameTime, errorCount, questions} = this.props;
-    return <WelcomeScreen
-      time={gameTime}
-      errorCount={errorCount}
-      onStartButtonClick = {() => {
-        this.setState((prevState) => ({
-          ...prevState,
-          question: prevState.question + 1,
-        }))
-      }}
-    />;
+    const {question} = this.state;
+    return App.getScreen(question, this.props, () => {
+      this.setState((prevState) => ({
+        question: prevState.question + 1,
+      }));
+    });
   }
 }
 
 App.propTypes = {
   gameTime: PropTypes.number,
   errorCount: PropTypes.number,
-  questions: PropTypes.object
+  questions: PropTypes.array
 };
 
 export default App;
+
+
+//
